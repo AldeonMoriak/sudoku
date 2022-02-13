05d71966b8b4a988a4b55c32c99e3baad1d0f34a
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2022-02-13 22:55:12 +0330

    add delete button

--- src/components/InputNumbers.svelte
+++ src/components/InputNumbers.svelte
@@ -1,39 +1,52 @@
 <script lang="ts">
-import type { Cell } from "src/types/Cell";
+  import type { Cell } from "src/types/Cell";
 
-import type { Lang } from "src/types/Lang";
-
+  import type { Lang } from "src/types/Lang";
 
   export let isNoteEnabled: boolean;
   export let noteToggleHandler: () => void;
   export let lang: Lang;
   export let numKeys: number[];
-  export let selectedCell: [number,number];
+  export let selectedCell: [number, number];
   export let rows: Cell[][];
   export let fillCellHandler: (num: number) => void;
+  export let deleteHandler: () => void;
 </script>
 
 <div dir="rtl" class="max-w-[400px] mx-auto grid grid-cols-9 text-center mt-2">
   <div
+    on:click={deleteHandler}
+    class="border-2 rounded-lg py-2 text-xl cursor-pointer flex justify-center bg-gray-700 text-white  border-white"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-7 w-7"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M6.707 4.879A3 3 0 018.828 4H15a3 3 0 013 3v6a3 3 0 01-3 3H8.828a3 3 0 01-2.12-.879l-4.415-4.414a1 1 0 010-1.414l4.414-4.414zm4 2.414a1 1 0 00-1.414 1.414L10.586 10l-1.293 1.293a1 1 0 101.414 1.414L12 11.414l1.293 1.293a1 1 0 001.414-1.414L13.414 10l1.293-1.293a1 1 0 00-1.414-1.414L12 8.586l-1.293-1.293z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </div>
+  <div
     class={` border-2 rounded-lg py-2  text-xl cursor-pointer flex justify-center ${
       isNoteEnabled
         ? "bg-yellow-200 text-gray-800 border-gray-800"
-        : "bg-gray-800 text-white  border-white"
+        : "bg-gray-700 text-white  border-white"
     }`}
     on:click={noteToggleHandler}
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
+      class="h-7 w-7"
+      viewBox="0 0 20 20"
       fill="currentColor"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+        d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
       />
     </svg>
   </div>
--- src/lib/Sudoku.svelte
+++ src/lib/Sudoku.svelte
@@ -51,26 +51,60 @@
     ];
   };
 
-  const puzzles = {
+  type Puzzle = {
+    puzzleGrid: number[];
+    clearGrid: number[];
+  };
+  type PuzzleCollection = {
+    easy: Puzzle[];
+    hard: Puzzle[];
+  };
+
+  const puzzles: PuzzleCollection = {
     easy: [
-      [
-        0, 2, 4, 0, 0, 0, 0, 9, 3, 0, 1, 0, 3, 0, 2, 0, 6, 0, 3, 8, 6, 1, 9, 0,
-        5, 2, 4, 0, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 1, 2, 0, 3, 1,
-        0, 0, 5, 7, 0, 0, 1, 0, 0, 9, 2, 0, 4, 3, 0, 0, 0, 3, 0, 5, 0, 2, 0, 6,
-        0, 5, 0, 0, 7, 0, 0, 8, 1,
-      ],
+      {
+        puzzleGrid: [
+          9, 5, 1, 4, 3, 2, 8, 6, 7, 4, 7, 2, 6, 5, 8, 1, 9, 3, 6, 3, 8, 9, 1,
+          7, 5, 2, 4, 8, 1, 7, 2, 9, 6, 4, 3, 5, 3, 9, 4, 5, 8, 1, 2, 7, 6, 2,
+          6, 5, 7, 4, 3, 9, 8, 1, 1, 2, 3, 8, 6, 5, 7, 4, 9, 5, 8, 9, 3, 7, 4,
+          6, 1, 2, 7, 4, 6, 1, 2, 9, 3, 5, 8,
+        ],
+        clearGrid: [
+          9, 5, 0, 0, 0, 2, 0, 6, 7, 4, 7, 2, 0, 0, 0, 1, 0, 3, 0, 3, 8, 9, 0,
+          0, 0, 2, 4, 0, 0, 7, 2, 0, 0, 0, 3, 5, 3, 0, 0, 5, 8, 1, 0, 7, 0, 0,
+          0, 0, 0, 0, 3, 9, 0, 0, 0, 2, 0, 8, 0, 0, 0, 0, 0, 0, 8, 9, 0, 7, 0,
+          0, 1, 2, 0, 0, 6, 0, 2, 0, 3, 5, 8,
+        ],
+      },
+      {
+        clearGrid: [
+          6, 0, 0, 0, 0, 9, 0, 0, 2, 0, 4, 3, 1, 0, 2, 0, 9, 0, 2, 0, 1, 0, 0,
+          8, 0, 6, 4, 9, 0, 0, 0, 0, 0, 8, 7, 1, 8, 0, 5, 7, 2, 0, 3, 0, 0, 4,
+          3, 7, 0, 0, 1, 2, 0, 6, 0, 7, 0, 0, 5, 0, 6, 0, 0, 0, 0, 6, 9, 0, 3,
+          4, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 5,
+        ],
+        puzzleGrid: [
+          6, 5, 8, 4, 7, 9, 1, 3, 2, 7, 4, 3, 1, 6, 2, 5, 9, 8, 2, 9, 1, 5, 3,
+          8, 7, 6, 4, 9, 6, 2, 3, 4, 5, 8, 7, 1, 8, 1, 5, 7, 2, 6, 3, 4, 9, 4,
+          3, 7, 8, 9, 1, 2, 5, 6, 1, 7, 9, 2, 5, 4, 6, 8, 3, 5, 8, 6, 9, 1, 3,
+          4, 2, 7, 3, 2, 4, 6, 8, 7, 9, 1, 5,
+        ],
+      },
     ],
     hard: [
-      [
-        3, 0, 6, 5, 0, 8, 4, 0, 0, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 0, 0, 0,
-        0, 3, 1, 0, 0, 3, 0, 1, 0, 0, 8, 0, 9, 0, 0, 8, 6, 3, 0, 0, 5, 0, 5, 0,
-        0, 9, 0, 6, 0, 0, 1, 3, 0, 0, 0, 0, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 7, 4,
-        0, 0, 5, 2, 0, 6, 3, 0, 0,
-      ],
+      {
+        puzzleGrid: [
+          3, 0, 6, 5, 0, 8, 4, 0, 0, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 0, 0,
+          0, 0, 3, 1, 0, 0, 3, 0, 1, 0, 0, 8, 0, 9, 0, 0, 8, 6, 3, 0, 0, 5, 0,
+          5, 0, 0, 9, 0, 6, 0, 0, 1, 3, 0, 0, 0, 0, 2, 5, 0, 0, 0, 0, 0, 0, 0,
+          0, 7, 4, 0, 0, 5, 2, 0, 6, 3, 0, 0,
+        ],
+        clearGrid: [],
+      },
     ],
   };
 
-  const selectedPuzzle = puzzles.easy[0];
+  const selectedPuzzle = puzzles.easy[0].clearGrid;
   const getPuzzleReady = (p: number[]) => {
     const puzzle: number[][] = [[], [], [], [], [], [], [], [], []];
     p.forEach((num, index) => {
@@ -231,38 +265,41 @@
       } else if (name === "Escape") {
         selectedCell = [-1, -1];
         return;
-      } else if (
-        (name === "Delete" || name === "BackSpace") &&
-        !isFixed &&
-        !isNoteEnabled
-      ) {
-        const cell = rows[selectedCell[0]][selectedCell[1]];
-        if (cell.notes.length) {
-          actions = [
-            ...actions,
-            {
-              isNote: true,
-              position: [...selectedCell],
-              value: [...cell.notes],
-            },
-          ];
-        } else {
-          actions = [
-            ...actions,
-            {
-              isNote: false,
-              position: [...selectedCell],
-              value: cell.value,
-            },
-          ];
-        }
-        rows[selectedCell[0]][selectedCell[1]].value = 0;
-        rows[selectedCell[0]][selectedCell[1]].notes = [];
-        redoActions = [];
+      } else if (name === "Delete" || name === "BackSpace") {
+        deleteHandler();
       } else if (name === "n") {
         isNoteEnabled = !isNoteEnabled;
       }
     }
+  };
+
+  const deleteHandler = () => {
+    const cell = rows[selectedCell[0]][selectedCell[1]];
+    if (cell.isFixed) {
+      return;
+    }
+    if (cell.notes.length) {
+      actions = [
+        ...actions,
+        {
+          isNote: true,
+          position: [...selectedCell],
+          value: [...cell.notes],
+        },
+      ];
+    } else {
+      actions = [
+        ...actions,
+        {
+          isNote: false,
+          position: [...selectedCell],
+          value: cell.value,
+        },
+      ];
+    }
+    rows[selectedCell[0]][selectedCell[1]].value = 0;
+    rows[selectedCell[0]][selectedCell[1]].notes = [];
+    redoActions = [];
   };
 
   const actionHandler = (action: Action) => {
@@ -371,7 +408,7 @@
 </script>
 
 <Header
-  menuClickHandler={menuClickHandler}
+  {menuClickHandler}
   {isMenuShown}
   {lang}
   {isTimerShown}
@@ -403,7 +440,7 @@
     {leftTime[0] === "00" ? leftTime.slice(1).join(":") : leftTime.join(":")}
   </div>
 </div>
-<div class="h-[50px]" />
+<div class="h-[30px]" />
 
 <Board
   {lang}
@@ -415,6 +452,7 @@
 />
 
 <InputNumbers
+  {deleteHandler}
   {isNoteEnabled}
   {noteToggleHandler}
   {lang}
@@ -423,4 +461,3 @@
   {rows}
   {fillCellHandler}
 />
-
